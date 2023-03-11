0d6f875c9cb410490d32ce6b1f3f1511874ba364
rirungayu <38273760+rirungayu@users.noreply.github.com>
2023-03-11 23:42:34 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,5 +9,23 @@
  */
 int check_cycle(const listint_t llist)
 {
+  listint_t *head;
+	listint_t *tail;
 
+	if (list == NULL)
+		return (0);
+
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
+	{
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
+			return (1);
+
+	}
+	return (0);
 }
